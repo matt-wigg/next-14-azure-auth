88598cadce2f85e882c714eb6c43d079c03494c7
Matthew Wigglesworth <62004884+matt-wigg@users.noreply.github.com>
2025-01-04 23:37:26 -0700

    Update and rename msGraphApi.ts to msGraph.ts

--- src/services/msGraph.ts
+++ src/services/msGraph.ts
@@ -2,11 +2,14 @@
 
 import { Client } from '@microsoft/microsoft-graph-client';
 
-export async function getUserDetails(accessToken: string) {
-  // https://learn.microsoft.com/en-us/graph/sdks/create-client?tabs=typescript
-  const client = Client.init({
+const initializeGraphClient = (accessToken: string) => {
+  return Client.init({
     authProvider: (done) => done(null, accessToken),
   });
+};
+
+export async function getUserDetails(accessToken: string) {
+  const client = initializeGraphClient(accessToken);
   const userDetails = await client.api('/me').get();
   return userDetails;
 }
